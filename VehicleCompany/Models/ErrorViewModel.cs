ad1c27a56e419f7990e3a0f5da1d3d79c7ff87df
der11111112@yandex.ru <der11111112@yandex.ru>
2025-12-03 02:04:11 +0300

    Создан проект

---
+++ VehicleCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
